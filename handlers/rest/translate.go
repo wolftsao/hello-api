87371c698697455cbc2831487346730a12bcf7e0
nathan <nathan@innotech.me>
2023-08-18 11:52:01 +0800

    fix ineffectual assignment

--- handlers/rest/translate.go
+++ handlers/rest/translate.go
@@ -28,7 +28,6 @@ func TranslateHandler(w http.ResponseWriter, r *http.Request) {
 	word := strings.ReplaceAll(r.URL.Path, "/", "")
 	translation := translation.Translate(word, language)
 	if translation == "" {
-		language = ""
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
